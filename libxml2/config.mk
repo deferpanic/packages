8f23c7eddf93148ea06b0e5c68fb6a0569075579
vsukhin <vsukhin@virtuososoft.com>
2016-04-29 16:11:45 +0300

    Removing cgi

--- libxml2/config.mk
+++ libxml2/config.mk
@@ -3,4 +3,5 @@
 #
 
 # Set the language specific configuration options for libxml2 
-PHP5_CONFIGURE_OPTS := --with-libxml-dir=$(RUMPRUN_PKGS_DIR)
+PHP5_ENABLE_CONFIGURE_OPTS := --with-libxml-dir=$(RUMPRUN_PKGS_DIR)
+PHP5_DISABLE_CONFIGURE_OPTS := --disable-libxml --disable-dom --disable-simplexml --disable-xml --disable-xmlreader --disable-xmlwriter --without-pear
--- php5/examples/run_nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -x
-rumprun xen -di -M 128 \
-    -I net1,xenif -W net1,inet,static,10.0.120.200/24 \
-    -b ../images/data.iso,/data \
-    -- ../../nginx/bin/nginx.bin -c /data/conf/nginx.conf
